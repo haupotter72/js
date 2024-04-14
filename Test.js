1fb3117b030b4789ff9479284326e0caf14a8bc7
ThoDev_Potter <93704520+haupotter72@users.noreply.github.com>
2024-04-14 22:00:21 +0700

    Dealine_ array

--- Test.js
+++ Test.js
@@ -1,142 +1,76 @@
-// truy cap vao array
-const actor = ["potter", "harry", "ron"];
-document.getElementById("demo").innerHTML = actor;
 
-// chuyen array sang string
-const Animals = ["dog", "cho", "meo", "cat"];
-document.getElementById("demo").innerHTML = Animals.toString();
 
-// get length cua array
 
-let size = Animals.length;
-document.getElementById("demo").innerHTML = size;
+// Our array with items
+const colors = ["red", "green", "blue"];
 
-// truy cap phan tu dau tien va cuoi cung cua array
+// Using length property
+const lengthWay = colors[colors.length - 2];
+console.log(lengthWay); // 'green'
 
-let animal1 = Animals[0];
-let animal2 = Animals[Animals.length - 1];
+// Using slice() method. Note an array is returned
+const sliceWay = colors.slice(-2, -1);
+console.log(sliceWay[0]); // 'green'
 
-//lap qua mot mang bang for
+// Using at() method
+const atWay = colors.at(-2);
+console.log(atWay); // 'green'
 
-let text = "<ul>";
-Animals.forEach(myFunction);
-text += "</ul>";
 
-document.getElementById("demo").innerHTML = text;
+const letters = ["a", "b", "c"];
+const numbers = [1, 2, 3];
 
-function myFunction(value) {
-  text += "<li>" + value + "</li>";
+const alphaNumeric = letters.concat(numbers);
+console.log(alphaNumeric);
+// results in ['a', 'b', 'c', 1, 2, 3]
+
+
+const num1 = [1, 2, 3];
+const num2 = [4, 5, 6];
+const num3 = [7, 8, 9];
+
+const numbers = num1.concat(num2, num3);
+
+console.log(numbers);
+// results in [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const letters = ["a", "b", "c"];
+
+const alphaNumeric = letters.concat(1, [2, 3]);
+
+console.log(alphaNumeric);
+// results in ['a', 'b', 'c', 1, 2, 3]
+function isBigEnough(element, index, array) {
+  return element >= 10;
 }
+[12, 5, 8, 130, 44].every(isBigEnough); // false
+[12, 54, 18, 130, 44].every(isBigEnough); // true
 
-// them phan tu array
-Animals.push("kitty");
+const array1 = [5, 12, 8, 130, 44];
 
-// nhan biet array
+const found = array1.find((element) => element > 10);
 
-let type = typeof Animals;
+console.log(found);
+// Expected output: 12
 
-// cac phuong thuc cua array
+const array1 = ['a', 'b', 'c'];
 
-let person = Animals.at(2);
+array1.forEach((element) => console.log(element));
 
-document.getElementById("demo").innerHTML = Animals.join(" * ");
+// Expected output: "a"
+// Expected output: "b"
+// Expected output: "c"
 
-Animals.pop();
-
-Animals.shift(); // bo phan tu dau
-
-document.getElementById("demo1").innerHTML =
-  "The first animal is: " + Animals[0];
-
-delete Animals[0];
-// sap nhap 2 mang
-const myGirls = ["Cecilie", "Lone"];
-const myBoys = ["Emil", "Tobias", "Linus"];
-const myChildren = myGirls.concat(myBoys);
-
-document.getElementById("demo").innerHTML = myChildren;
-// copy phan tu vao mang:
-
-document.getElementById("demo").innerHTML = Animals.copyWithin(2, 0);
-
-// them cac muc vao phan tu cua mang
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-document.getElementById("demo1").innerHTML = fruits;
-
-fruits.splice(2, 0, "Lemon", "Kiwi");
-document.getElementById("demo2").innerHTML = fruits;
-
-let position = fruits.indexOf("Apple") + 1;
-
-document.getElementById("demo").innerHTML = "Apple is found in position " + position;
+const arr = [];
+arr.push(1, [3, arr, 4], 2);
+console.log(arr.join(";")); // 1;3,,4;2
 
 
-// array sort
-const points = [40, 100, 1, 5, 25, 10];
-document.getElementById("demo").innerHTML = points;
+const a = ["Wind", "Water", "Fire"];
+a.join(); // 'Wind,Water,Fire'
+a.join(", "); // 'Wind, Water, Fire'
+a.join(" + "); // 'Wind + Water + Fire'
+a.join(""); // 'WindWaterFire'
 
-function myFunction1() {
-  points.sort();
-  document.getElementById("demo").innerHTML = points;
-}
-
-function myFunction2() {
-  points.sort(function(a, b){return a - b});
-  document.getElementById("demo").innerHTML = points;
-}
-
-const points = [40, 100, 1, 5, 25, 10];
-points.sort(function(){return 0.5 - Math.random()});
-
-
-const points1 = [40, 100, 1, 5, 25, 10];
-
-for (let i = points.length -1; i > 0; i--) {
-  let j = Math.floor(Math.random() * (i+1));
-  let k = points[i];
-  points[i] = points[j];
-  points[j] = k;
-}
-// tim so thap nhat trong mang:
-function myArrayMin(arr) {
-  return Math.min.apply(null, arr);
-}
-
-// tim so cao nhat trong mang:function myArrayMax(arr) {
-function myArrayMax(arr) {
-  return Math.max.apply(null, arr);
-}
-
-function myArrayMax(arr) {
-  return Math.max.apply(null, arr);
-}
-
-// nhan 2 cho tung phan tung trong mang : const numbers1 = [45, 4, 9, 16, 25];
-const numbers2 = numbers1.map(myFunction);
-
-document.getElementById("demo").innerHTML = numbers2;
-
-function myFunction(value, index, array) {
-  return value * 2;
-}
-// chon ra phan tu > so duoc chon trong mang
-const numbers = [45, 4, 9, 16, 25];
-const over18 = numbers.filter(myFunction);
-
-document.getElementById("demo").innerHTML = over18;
-
-function myFunction(value, index, array) {
-  return value > 18;
-}
-
-// tong cac phan tu trong mang
-const numbers = [45, 4, 9, 16, 25];
-let sum = numbers.reduce(myFunction);
-
-document.getElementById("demo").innerHTML = "The sum is " + sum;
-
-function myFunction(total, value, index, array) {
-  return total + value;
-}
 
 
